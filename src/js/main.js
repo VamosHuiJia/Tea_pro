4be4b0e3c11402028438691359d31d5a379f59e9
VamosHuiJia <giahuy20092004@gmail.com>
2026-01-25 11:12:44 +0700

    complete navbar + hamburger menu

--- src/js/main.js
+++ src/js/main.js
@@ -16,25 +16,32 @@ $(function () {
     }
 });
 
+// Navigation
+$(function () {
+    // Ẩn hiện navigation
+    $(".navbar").hidescroll();
 
-// Ẩn hiện navigation
-$(function() {
-    $('.navbar').hidescroll();
-})
+    // Hamburger menu
+    const toggleBtn = $("#toggle_btn");
+    const dropdownMenu = $(".dropdown-menu");
 
+    toggleBtn.click(() => {
+        dropdownMenu.toggleClass("open");
+    });
+});
 
 // Đổi qua các tab Products
 $(function () {
-    // Active tab cho li đầu
+    // Active tab cho <li> đầu
     $("li:first").addClass("activeTab");
 
     // đổi màu Active tab
-    $("li").on("click", function() {
+    $("li").on("click", function () {
         $("li").removeClass("activeTab");
         $('div[id="products-tabs"] ul .r-tabs-state-active').addClass("activeTab");
-    })
+    });
 
     $("#products-tabs").responsiveTabs({
-        animation: 'fade',
+        animation: "fade",
     });
 });
