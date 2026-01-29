36a6f9e44ba5a1cf4f93526a41d91aaffa5ccf61
VamosHuiJia <giahuy20092004@gmail.com>
2026-01-29 16:57:24 +0700

    counter up data

--- src/js/main.js
+++ src/js/main.js
@@ -47,9 +47,33 @@ $(function () {
 });
 
 // Slicker sản phẩm
-$(function() {
+$(function () {
     $(".slider").slick({
         autoplay: true,
         dots: true,
-    })
-})
+    });
+});
+
+// counter up
+$(function () {
+    const counterUp = window.counterUp.default;
+
+    const callback = (entries) => {
+        entries.forEach((entry) => {
+            const el = entry.target;
+            if (entry.isIntersecting && !el.classList.contains("is-visible")) {
+                counterUp(el, {
+                    duration: 2000,
+                    delay: 16,
+                });
+                el.classList.add("is-visible");
+            }
+        });
+    };
+
+    const IO = new IntersectionObserver(callback, { threshold: 1 });
+
+    const el = document
+        .querySelectorAll(".counter")
+        .forEach((node) => IO.observe(node));
+});
