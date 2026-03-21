4d739c09b0e18392b284bb0ee8c0783dd7a7ad3a
VamosHuiJia <giahuy20092004@gmail.com>
2026-03-21 21:16:36 +0700

    commit

--- src/api/admin/brand.api.ts
+++ src/api/admin/brand.api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = `${import.meta.env.REACT_APP_API_URL || 'http://localhost:5000'}/api`;
+const API_BASE_URL = `${import.meta.env.REACT_APP_API_URL}/api`;
 
 export const getAllBrands = async () => {
     const response = await fetch(`${API_BASE_URL}/brands/all-brands`);
